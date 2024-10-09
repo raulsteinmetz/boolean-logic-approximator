40543481307bcbfbf7be1fdea7aa3412d31b2d7f
raulsteinmetz <rsteinmetz@inf.ufsm.br>
2024-10-09 14:14:44 -0300

    random boolean expression generator

--- data/data_gen.py
+++ data/data_gen.py
@@ -13,11 +13,7 @@ def _and(p: bool, q: bool):
 def _or(p: bool, q: bool):
     return p or q
 
-def _not(p: bool):
-    return not p
-
-op_map = {'not': _not,
-          'and': _and,
+op_map = {'and': _and,
           'or': _or,
           'if_then': _if_then}
 
@@ -26,23 +22,32 @@ op_map = {'not': _not,
     number of variables and possible gates are parameterized.
 '''
 
-def randomize_ops(ops: list, n_variables: int, n_ops: int):
-    nots = np.zeros(n_variables, dtype=bool)
+def create_exp(ops: list, n_variables: int, n_ops: int):
+    nots = np.zeros(n_ops, dtype=bool)
     if 'not' in ops:
         ops.remove('not')
-        nots = np.random.choice([True, False], n_variables)
-
+        nots = np.random.choice([True, False], n_ops)
+    
     ops = [np.random.choice(ops) for _ in range(n_ops)]
+    order_of_variables = [np.random.choice(range(n_variables)) for _ in range(n_ops)]
 
-    def expression(variables: list, nots, ops):
-        # compute nots (invert variables in idx where nots are true)
-        # compute expression, return value
-        pass
+    def exp_f(x: list):
+        variables = [x[i] for i in order_of_variables]
+        variables = [not v if n else v for v, n in zip(variables, nots)]
+        value = variables[0]
+        for i in range(1, n_ops):
+            value = op_map[ops[i-1]](value, variables[i])
+        return value
+
+    exp_s = f"v{order_of_variables[0]}" if nots[0] == False else f"~v{order_of_variables[0]}"
+    for i in range(1, n_ops):
+        exp_s += f" {ops[i-1]} " + (f"~v{order_of_variables[i]}" if nots[i] else f"v{order_of_variables[i]}")
+
+    return exp_f, exp_s
 
 def generate(ops: list, n_variables: int, n_ops: int):
-    expression = randomize_ops(ops, n_variables, n_ops)
-
-
+    expression, exp_s = create_exp(ops, n_variables, n_ops)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pass the list of \
